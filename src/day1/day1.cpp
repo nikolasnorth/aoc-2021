c06fc5d5777ef49ca0749275a711f152446e6be0
Nikolas North <nikolasn09@gmail.com>
2021-12-31 14:00:28 -0500

    refactor day1, part2

--- src/day1/day1.cpp
+++ src/day1/day1.cpp
@@ -19,16 +19,12 @@ namespace aoc2021::day1 {
   auto Part2(const std::string &filename) -> unsigned int {
     auto lines = util::ReadLines(filename);
     unsigned int count = 0;
-    int prev = INT_MAX;
-    for (auto line = lines.begin() + 2; line != lines.end(); ++line) {
-      auto a = std::stoi(*(line - 2));
-      auto b = std::stoi(*(line - 1));
-      auto c = std::stoi(*line);
-      auto sum = a + b + c;
-      if (prev < sum) {
+    for (auto line = lines.begin() + 3; line != lines.end(); ++line) {
+      auto prev = std::stoi(*(line - 3)) + std::stoi(*(line - 2)) + std::stoi(*(line - 1));
+      auto current = std::stoi(*(line - 2)) + std::stoi(*(line - 1)) + std::stoi(*line);
+      if (prev < current) {
         ++count;
       }
-      prev = sum;
     }
     return count;
   }
