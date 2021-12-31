2b526d32402ddccc609e81f8c86dcf6edcf6571f
Nikolas North <nikolasn09@gmail.com>
2021-12-31 14:03:51 -0500

    change comment and remove using

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #include "day1/day1.h"
 
 int main() {
-  using namespace aoc2021;
   const std::string &filename = "../input.txt";
-  std::cout << day1::Part2(filename) << '\n';
+  std::cout << aoc2021::day1::Part2(filename) << '\n';
 }
