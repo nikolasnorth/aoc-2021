193ea1f3f286ab4a4a6e42bf4bc045b52ea64515
Nikolas North <nikolasn09@gmail.com>
2021-12-31 11:42:33 -0500

    complete day1, part1

---
+++ src/day1.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+#include <iostream>
+
+namespace day1 {
+  auto Part1(const std::string &filename) -> unsigned int;
+}
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <iostream>
+
+#include "day1.h"
+
+int main() {
+  const std::string &filename = "../input.txt";
+  std::cout << day1::Part1(filename) << '\n';
+}
